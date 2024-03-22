c2e3d8de03aeb904cb16d0b641a3962482ad1d32
Siddharth Karanchery <webdev.siddharth@gmail.com>
2024-03-22 22:57:03 +0530

    bug fix for build

--- src/App.js
+++ src/App.js
@@ -4,28 +4,15 @@ import "./App.css";
 function App() {
   // require("dotenv").config();
 
-  console.log("process.env: ".process.env);
-  console.log("process.env.TEST_ENV_VAR_STR: ".process.env.TEST_ENV_VAR_STR);
-  console.log("process.env.TEST_ENV_VAR_NUM: ".process.env.TEST_ENV_VAR_NUM);
+  console.log("process.env: ", process.env);
+  console.log("process.env.TEST_ENV_VAR_STR: ", process.env.TEST_ENV_VAR_STR);
+  console.log("process.env.TEST_ENV_VAR_NUM: ", process.env.TEST_ENV_VAR_NUM);
 
   console.log("test");
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>This is a test app</p>
     </div>
   );
 }
